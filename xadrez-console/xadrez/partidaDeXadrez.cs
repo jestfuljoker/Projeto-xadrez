cf50b23145611b341b0ceb5d676cb8274c303725
Christofer Assis <chris.f.assis18@gmail.com>
2021-02-14 20:59:19 -0300

    jogada especial promocao

--- xadrez-console/xadrez/partidaDeXadrez.cs
+++ xadrez-console/xadrez/partidaDeXadrez.cs
@@ -142,6 +142,21 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em Xeque!");
             }
+
+            Peca p = Tab.Peca(destino);
+
+            // #jogadaespecial promocao
+            if (p is Peao)
+            {
+                if ((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.retirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
             if (estaEmXeque(adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -159,8 +174,6 @@ namespace xadrez
                 Turno++;
                 mudaJogador();
             }
-
-            Peca p = Tab.Peca(destino);
 
             // #jogadaespecial en passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
